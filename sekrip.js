5565513e3f9a870fe148c40fd06237003cecb89f
Unknown-Dev7 <unknowndev896@gmail.com>
2025-03-29 09:09:10 +0700

    Add files via upload

---
+++ sekrip.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("content").classList.remove("hidden");
+    }, 2000);
+});
